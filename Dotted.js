0cb721d23241ac399e47feb728c42052d388a9a0
SnoozeGruntSilver420 <lesilversaiyan@gmail.com>
2017-07-18 17:19:06 -0500

    lols

--- Dotted.js
+++ Dotted.js
@@ -2,14 +2,41 @@
 var theBody = document.querySelector("body");
 
 
+function setDivColor(element) {
+    var redBackground = getRandomInt(0,255);
+    var greenBackground = getRandomInt(0,255);
+    var blueBackground = getRandomInt(0,255);
+    element.style.backgroundColor = `rgb(${redBackground},${greenBackground},${blueBackground})`;
+}
+
+
 var counter = 0;
 
-while(counter < 10 ) {
+while(counter < 10) {
   var theDiv = document.createElement("div");
 
-    theDiv.style.backgroundColor = "silver";
-
-    theBody.appendChild (theDiv);
+  setDivColor(theDiv);
+ 
+  theBody.appendChild(theDiv);
   
-    counter++;
+  counter++;
 }
+
+
+function getRandomInt(min,max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var divs = document.querySelectorAll("div");
+
+ divs.forEach( function(div) {
+   div.addEventListener("mouseover", function(){
+      setDivColor(div);
+   })
+ })
+
+
+
+
+
+
